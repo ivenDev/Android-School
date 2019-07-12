77c641063284098e789a087d8b690a247de237c4
Cloniamix Home <cloniamix@mail.ru>
2019-07-12 12:35:04 +0300

    Lesson 9:  - init

---
+++ Lesson_9_Engurazov_Kotlin/app/src/main/java/com/cloniamix/lesson_9_engurazov_kotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.cloniamix.lesson_9_engurazov_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
